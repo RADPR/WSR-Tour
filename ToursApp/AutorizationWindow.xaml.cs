cc7429466fd17f1b522704c59edd7055b81018c7
RAD <RAD@RADPC>
2023-01-28 00:17:07 +0300

    Добавьте файлы проекта.

---
+++ ToursApp/AutorizationWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AutorizationWindow.xaml
+    /// </summary>
+    public partial class AutorizationWindow : Window
+    {
+        public AutorizationWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Avtorizacia_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+            MessageBox.Show("Вы успешно прошли авторизацию");   
+        }
+    }
+}
